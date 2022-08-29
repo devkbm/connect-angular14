fef5c43d9702b6ff3de45e28089b99f0f13ef8e7
devkbm <devkbm0417@gmail.com>
2022-08-29 21:12:34 +0900

    2022-08-29

--- src/app/common/user/user.component.ts
+++ src/app/common/user/user.component.ts
@@ -5,6 +5,7 @@ import { AppBase } from '../../core/app/app-base';
 import { UserService } from './user.service';
 import { ResponseObject } from 'src/app/core/model/response-object';
 import { User } from './user.model';
+import { ButtonTemplate } from 'src/app/shared/nz-buttons/nz-buttons.component';
 
 @Component({
   selector: 'app-user',
@@ -15,6 +16,36 @@ export class UserComponent extends AppBase implements OnInit {
 
   @ViewChild(UserGridComponent, {static: true})
   grid!: UserGridComponent;
+
+  buttons: ButtonTemplate[] = [{
+    text: '구글 로그인',
+    nzType: 'search',
+    click: (e: MouseEvent) => {
+      this.test();
+    }
+  },{
+    text: '조회',
+    nzType: 'search',
+    click: (e: MouseEvent) => {
+      this.getUserList();
+    }
+  },{
+    text: '신규',
+    nzType: 'form',
+    click: (e: MouseEvent) => {
+      this.newForm();
+    }
+  },{
+    text: '삭제',
+    nzType: 'delete',
+    isDanger: true,
+    popConfirm: {
+      title: '삭제하시겠습니까?',
+      confirmClick: () => {
+        this.deleteUser();
+      }
+    }
+  }];
 
   drawerVisible = false;
 
--- src/app/shared/nz-buttons/nz-buttons.component.ts
+++ src/app/shared/nz-buttons/nz-buttons.component.ts
@@ -2,13 +2,13 @@ import { Component, EventEmitter, Input, OnInit } from '@angular/core';
 
 export interface ButtonTemplate {
   text: string;
-  click(evt: MouseEvent): void;
+  click?(evt: MouseEvent): void;
   nzType?: string;
   isDanger?: boolean;
   popConfirm?: {
     title: string;
     confirmClick(): void;
-    cancelClick(): void;
+    cancelClick?(): void;
   }
 }
 
@@ -49,12 +49,12 @@ export interface ButtonTemplate {
   template: `
     <nz-button-group *ngFor="let btn of buttons; let i = index">
       <!-- nz-popconfirm을 사용하지 않을 경우 -->
-      <button nz-button (click)="btn.click($event)" [nzDanger]="btn.isDanger" *ngIf="btn.popConfirm === null">
+      <button nz-button (click)="btn?.click($event)" [nzDanger]="btn.isDanger" *ngIf="btn.popConfirm === null">
         <i nz-icon [nzType]="btn.nzType" nzTheme="outline" *ngIf="btn.nzType"></i>{{btn.text}}
       </button>
 
       <!-- nz-popcofirm을 사용할 경우 -->
-      <button nz-button (click)="btn.click($event)" [nzDanger]="btn.isDanger" *ngIf="btn.popConfirm !== null"
+      <button nz-button (click)="btn?.click($event)" [nzDanger]="btn.isDanger" *ngIf="btn.popConfirm !== null"
         nz-popconfirm [nzPopconfirmTitle]="btn.popConfirm?.title" [nzOkType]="btn.isDanger === true ? 'danger' : 'primary'"
         (nzOnConfirm)="btn.popConfirm?.confirmClick()" (nzOnCancel)="btn.popConfirm?.cancelClick()">
         <i nz-icon [nzType]="btn.nzType" nzTheme="outline" *ngIf="btn.nzType"></i>{{btn.text}}
